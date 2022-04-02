a0b018404c12f048adda7ca6aee78c1866209fb7
Oleksandr Chudov <choodov@gmail.com>
2022-04-03 00:28:14 +0200

    Fix api response

--- giftcard/src/main/java/com/chudov/giftservice/giftcard/controller/GiftCardController.java
+++ giftcard/src/main/java/com/chudov/giftservice/giftcard/controller/GiftCardController.java
@@ -1,8 +1,11 @@
 package com.chudov.giftservice.giftcard.controller;
 
 import com.chudov.giftservice.giftcard.entity.GiftCardRequest;
+import com.chudov.giftservice.giftcard.exception.InsufficientBalanceException;
 import com.chudov.giftservice.giftcard.service.GiftCardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +19,14 @@ public class GiftCardController {
     private GiftCardService service;
 
     @PostMapping("/buy-gift")
-    public void buyGift(@RequestBody GiftCardRequest giftCardRequest) {
-        service.buyGifts(giftCardRequest);
+    public ResponseEntity buyGift(@RequestBody GiftCardRequest giftCardRequest) {
+        try {
+            service.buyGifts(giftCardRequest);
+        } catch (InsufficientBalanceException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
